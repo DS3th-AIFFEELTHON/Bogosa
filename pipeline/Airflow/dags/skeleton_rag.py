1a133bea3424552986b31044d2f11cbf732caef9
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2025-03-26 12:47:29 +0900

    chore: skeleton_rag.py 최신 버전으로 교체

--- pipeline/Airflow/dags/skeleton_rag.py
+++ pipeline/Airflow/dags/skeleton_rag.py
@@ -7,39 +7,52 @@ from uuid import uuid4
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_experimental.text_splitter import SemanticChunker
 import fitz
-from skeleton_main import get_file_paths, process_pdf, flatten_docs, generate_uuids, vectorstore, embeddings, text_splitter
+from skeleton_main import get_file_paths, preprocessing, open_jsonl, generate_uuids, create_vectorstore, add_docs
 
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-FILE_PATH = '/Users/hayan/airflow/dags/raw_pdf_1'
+FILE_PATH = '/Users/hayan/airflow/dags/raw_pdf'
 embeddings = OpenAIEmbeddings(model='text-embedding-3-large')
 text_splitter = SemanticChunker(embeddings)
 
 
 def process_pdf_files(ti, **kwargs):
-    file_paths = get_file_paths(FILE_PATH)
-    doc_600_100 = process_pdf(file_paths, embeddings, text_splitter)
-    flattened_list = flatten_docs(doc_600_100)
-    ti.xcom_push(key='flattened_list', value=flattened_list)
+    preprocessing(FILE_PATH)
+    text = open_jsonl(get_file_paths(FILE_PATH)[0].replace('.pdf', '_semantic_80.jsonl'))
+    table = open_jsonl(get_file_paths(FILE_PATH)[0].replace('.pdf', '_table_v7.jsonl'))
+    image = open_jsonl(get_file_paths(FILE_PATH)[0].replace('.pdf', '_image.jsonl'))
+    raptor = open_jsonl(get_file_paths(FILE_PATH)[0].replace('.pdf', '_raptor.jsonl'))
+
+    ti.xcom_push(key='text', value=text)
+    ti.xcom_push(key='table', value=table)
+    ti.xcom_push(key='image', value=image)
+    ti.xcom_push(key='raptor', value=raptor)
 
 def add_documents_to_vectorstore(ti, **kwargs):
-    flattened_list = ti.xcom_pull(key='flattened_list', task_ids='process_pdf_files')
-    uuids = generate_uuids(len(flattened_list))
-    vectorstore.add_documents(
-        documents=flattened_list,
-        embedding=embeddings,
-        ids=uuids
-    )
+    text = ti.xcom_pull(key='text', task_ids='process_pdf_files')
+    table = ti.xcom_pull(key='table', task_ids='process_pdf_files')
+    image = ti.xcom_pull(key='image', task_ids='process_pdf_files')
+    raptor = ti.xcom_pull(key='raptor', task_ids='process_pdf_files')
+    
+    vectorstore_text = create_vectorstore('airflow-text')
+    vectorstore_table = create_vectorstore('airflow-table')
+    vectorstore_image = create_vectorstore('airflow-image')
+    vectorstore_raptor = create_vectorstore('airflow-raptor')
+
+    add_docs(vectorstore_text, text)
+    add_docs(vectorstore_table, table)
+    add_docs(vectorstore_image, image)
+    add_docs(vectorstore_raptor, raptor)
 
 
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2025, 3, 20),
+    'start_date': datetime(2025, 3, 25),
 }
 
 dag = DAG(
